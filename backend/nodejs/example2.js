67c16d9c9df5eb725be305f2932b44818172b22f
nvirdi21 <virdinavnoor@gmail.com>
2025-09-03 18:01:46 +1000

    linting should fail for this

--- backend/nodejs/example2.js
+++ backend/nodejs/example2.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('some-database-module'); // This is a fake module for illustration
 
+let noor = "noor";
 // A highly vulnerable SQL injection endpoint
 router.get("/user", (req, res) => {
     // This is UNSAFE! User input is directly concatenated into the query
